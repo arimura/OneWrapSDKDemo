8fe2e7526227630605e35359394bc7986f6ea9c4
arimura <kotaro_arimura@voyagegroup.com>
2023-02-08 13:44:15 +0900

    Add project

---
+++ OneWrapSDKDemo/OneWrapSDKDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OneWrapSDKDemo
+//
+//  Created by k-arimura on 2023/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
